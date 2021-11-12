ed5bfd98518e47c426b1c0c770bda011c449c8ab
Irina-Golubitsky <irina.golubitsky@gmail.com>
2021-11-12 09:13:02 -0800

    shopping cart added

---
+++ client/build/precache-manifest.1f4e1d4ace60038ae96f5919b2683bc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e74d84e606beee34bc8425d386e771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8da5f22d7b2ace762bef",
+    "url": "/static/css/main.49b21ed8.chunk.css"
+  },
+  {
+    "revision": "cadb161247777d59c5be",
+    "url": "/static/js/2.5b048a87.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.5b048a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da5f22d7b2ace762bef",
+    "url": "/static/js/main.1c3d6d20.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
+//const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,14 +15,13 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
+server.start().then(res => {
+
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 // Serve up static assets
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -36,3 +36,4 @@ db.once('open', () => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
+})
